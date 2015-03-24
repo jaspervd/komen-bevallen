5330f93bcf21e14de0b664bc577579aa007ef767
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-03-25 00:27:41 +0100

    fixed public double map

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,41 @@
-/* globals __filename:true */
+/* globals __dirname:true */
 
-var gulp = require("gulp");
+var gulp = require('gulp');
+var path = require("path");
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var browserifyHandlebars = require('browserify-handlebars');
-var package_json = require('./package.json');
+var main_bower_files = require('main-bower-files');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var config = require('./config.json');
+var package_json = require('./package.json');
 
-var plugins = require("gulp-load-plugins")({
+var plugins = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*'],
     replaceString: /\bgulp[\-.]/,
 });
 
 function getFilePath(target, type) {
-    return config[type][target].folder + "/" + config[type][target].file;
+    return config[type][target].folder + '/' + config[type][target].file;
 }
 
-var scripts = [getFilePath("src", "scripts"), getFilePath("src", "shared"), package_json.main, __filename];
-
-//alias for size(), errors on .size.
-plugins.filesize = plugins.size;
+var scripts = [
+    "**/*.js",
+    "!node_modules/**/*.js",
+    "!public/**/*.js",
+    "!js/**/*.js",
+    "!bower_components/**/*.js"
+];
 
 gulp.task('stylesheets', function() {
-    return gulp.src(getFilePath("src", "stylesheets"))
+    return gulp.src([getFilePath('src', 'stylesheets'), '!_scss/_reset.scss'])
         .pipe(plugins.plumber())
-        .pipe(plugins.scssLint({
-            'config': '.scss-lint.yml'
-        }))
-        .pipe(plugins.compass({
-            css: config.stylesheets.dest.folder,
-            sass: config.stylesheets.src.folder,
+        .pipe(plugins.scssLint())
+        .pipe(plugins.sass({
+            includePaths: [config.stylesheets.src.folder]
         }))
         .pipe(plugins.autoprefixer({
             browsers: ['last 2 versions', 'ie 9'],
@@ -39,39 +43,87 @@ gulp.task('stylesheets', function() {
         }))
         .pipe(plugins.util.env.type === 'production' ? plugins.minifyCss() : plugins.util.noop())
         .pipe(plugins.filesize())
-        .pipe(gulp.dest(config.stylesheets.dest.folder));
+        .pipe(gulp.dest(config.stylesheets.dest.folder))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
-function jshint(paths) {
-    return gulp.src(paths)
-        .pipe(plugins.plumber())
-        .pipe(plugins.jshint("./.jshintrc"))
-        .pipe(plugins.jshint.reporter('jshint-stylish'))
-        .pipe(plugins.jshint.reporter("fail"))
-        .on('error', function(error) {
-            plugins.util.beep();
-        });
+function browserify_file(file) {
+    function rebundle() {
+
+        return bundler.bundle()
+            .on('error', function(error) {
+                console.log(error);
+                this.emit("end");
+            })
+            .pipe(source(path.basename(file)))
+            .pipe(buffer())
+            .pipe(plugins.util.env.type === 'production' ? plugins.uglify() : plugins.util.noop())
+            .pipe(plugins.filesize())
+            .pipe(gulp.dest(config.scripts.dest.folder));
+    }
+
+    var bundler = watchify(browserify(file, watchify.args));
+    bundler.on('update', rebundle);
+
+    return rebundle();
+
 }
 
 gulp.task("scripts", function() {
-    jshint(scripts);
-    return browserify("./" + config.scripts.src.folder + "/" + config.scripts.src.entry_file)
-        .transform(browserifyHandlebars)
-        .bundle()
+    return gulp.src(getFilePath('src', 'scripts'))
+        .pipe(plugins.tap(function(file) {
+            return browserify_file("./" + path.relative(__dirname, file.path));
+        }))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task("vendor", function() {
+    var js_filter = plugins.filter(['*.js']);
+    var css_filter = plugins.filter(['*.css']);
+    var fonts_filter = plugins.filter(['*.ttf', '*.eot', '*.svg', '*.woff']);
+
+    return gulp.src(main_bower_files())
+        .pipe(js_filter)
+        .pipe(plugins.uglify())
+        .pipe(plugins.concat(config.vendor.dest.file))
+        .pipe(gulp.dest(config.scripts.dest.folder))
+        .pipe(js_filter.restore())
+        .pipe(css_filter)
+        .pipe(plugins.minifyCss())
+        .pipe(gulp.dest(config.stylesheets.dest.folder))
+        .pipe(css_filter.restore())
+        .pipe(fonts_filter)
+        .pipe(gulp.dest(config.fonts.dest.folder));
+
+});
+
+gulp.task("hinting", function() {
+    return gulp.src(scripts)
+        .pipe(plugins.plumber())
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.jshint.reporter("fail"))
         .on('error', function(error) {
             this.emit("end");
-        })
-        .pipe(source(config.scripts.dest.file))
-        .pipe(buffer())
-        .pipe(plugins.util.env.type === 'production' ? plugins.uglify() : plugins.util.noop())
-        .pipe(plugins.filesize())
-        .pipe(gulp.dest(config.scripts.dest.folder));
+        });
 });
 
 gulp.task('watch', function() {
+    gulp.watch(getFilePath('src', 'vendor'), ['vendor']);
+    gulp.watch(getFilePath('src', 'scripts'), ['scripts']);
     gulp.watch(getFilePath("src", "stylesheets"), ['stylesheets']);
-    gulp.watch(getFilePath("src", "templates"), ['scripts']);
-    gulp.watch(scripts, ['scripts']);
+    gulp.watch(scripts, ['hinting']);
 });
 
-gulp.task('default', ['watch', 'stylesheets', 'scripts']);
+gulp.task('browser-sync', function() {
+    browserSync({
+        host: "localhost",
+        port: 8888
+    });
+});
+
+gulp.task('default', ['watch', 'stylesheets', 'hinting', 'scripts', 'vendor', 'browser-sync']);
--- public/index.php
+++ public/index.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Komen Bevallen</title>
-        <link rel="stylesheet" href="css/screen.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    </head>
-    <body>
-    <div class="container">
-    	<form method="post" action="#">
-
-    	</form>
-    </div>
-    <script type="text/javascript" src="js/backbone/backbone.js"></script>
-    <script type="text/javascript" src="js/handlebars/handlebars.min.js"></script>
-    <script type="text/javascript" src="js/jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="js/underscore/underscore.js"></script>
-    <script type="text/javascript" src="js/app.js"></script>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Komen Bevallen</title>
+	<link rel="stylesheet" href="css/screen.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body>
+	<script type="text/javascript" src="js/vendor.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type='text/javascript' id="__bs_script__">//<![CDATA[
+	document.write("<script async src='http://HOST:8890/browser-sync/browser-sync-client.2.4.0.js'><\/script>".replace("HOST", location.hostname));
+	//]]></script>
 </body>
 </html>
