6049ee6c8693b70fef39795e7754801394c8d6fd
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-03-26 22:01:27 +0100

    added delete for api/users so user can logout

--- api/index.php
+++ api/index.php
@@ -21,41 +21,16 @@ $app->get('/me/?', function() use ($usersDAO) {
         return Util::json(array());
     }
 });
-/*
-$app->post('/login/?', function () use ($app, $usersDAO) {
-    $post = $app->request->post();
-    if (empty($post)) {
-        $post = (array)json_decode($app->request()->getBody());
-    }
 
-    $auth = $usersDAO->authenticate($post['email'], $post['password']);
-    if (!empty($auth)) {
-        unset($auth['password']);
-        return Util::json($auth);
-    }
-    else {
-    	// return unauthorized http code
+$app->delete('/users/:id/?', function($id) {
+    if(!empty($_SESSION['komen_bevallen']['user']) && $_SESSION['komen_bevallen']['user']['id'] == $id) {
+        unset($_SESSION['komen_bevallen']['user']);
+        http_response_code(200);
+        exit;
+    } else {
+        // return bad request http code (zie https://tools.ietf.org/html/rfc7231#section-6.5.1)
         http_response_code(401);
-        return Util::json(array('error' => 'Foute logingegevens.'));
+        exit;
     }
 });
-
-$app->post('/users/?', function () use ($app, $usersDAO) {
-    $post = $app->request->post();
-    if (empty($post)) {
-        $post = (array)json_decode($app->request()->getBody());
-    }
-
-    $user = $usersDAO->register($post['email'], $post['password']);
-    if (!empty($user)) {
-        unset($user['password']);
-        return Util::json($user);
-    }
-    else {
-    	// return bad request http code (zie https://tools.ietf.org/html/rfc7231#section-6.5.1)
-        http_response_code(400);
-        return Util::json(array('error' => 'Er is iets verkeerd gegaan tijdens het registreren.'));
-    }
-});
-*/
 $app->run();
