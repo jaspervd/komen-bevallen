c8287201d6404b8e1dcbf17954d68ae192544a51
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-03-28 17:33:54 +0100

    changed tablename rate to ratings

--- maiv_komen.sql
+++ maiv_komen.sql
@@ -1,13 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.10
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost:8889
+-- Gegenereerd op: 28 mrt 2015 om 17:33
+-- Serverversie: 5.5.38
+-- PHP-versie: 5.6.2
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Databank: `maiv_komen`
+--
 CREATE DATABASE IF NOT EXISTS `maiv_komen` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `maiv_komen`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kb_admin`
+--
 
 DROP TABLE IF EXISTS `kb_admin`;
 CREATE TABLE `kb_admin` (
@@ -15,11 +34,23 @@ CREATE TABLE `kb_admin` (
   `finished` enum('yes','no') NOT NULL DEFAULT 'no'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kb_groups`
+--
+
 DROP TABLE IF EXISTS `kb_groups`;
 CREATE TABLE `kb_groups` (
 `id` int(11) NOT NULL,
   `week` tinyint(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kb_photos`
+--
 
 DROP TABLE IF EXISTS `kb_photos`;
 CREATE TABLE `kb_photos` (
@@ -30,8 +61,14 @@ CREATE TABLE `kb_photos` (
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `kb_rate`;
-CREATE TABLE `kb_rate` (
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kb_ratings`
+--
+
+DROP TABLE IF EXISTS `kb_ratings`;
+CREATE TABLE `kb_ratings` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `points_show` tinyint(2) NOT NULL,
@@ -39,6 +76,12 @@ CREATE TABLE `kb_rate` (
   `points_partner` tinyint(2) NOT NULL,
   `contender_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kb_users`
+--
 
 DROP TABLE IF EXISTS `kb_users`;
 CREATE TABLE `kb_users` (
@@ -53,26 +96,56 @@ CREATE TABLE `kb_users` (
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indexen voor geëxporteerde tabellen
+--
 
+--
+-- Indexen voor tabel `kb_groups`
+--
 ALTER TABLE `kb_groups`
  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexen voor tabel `kb_photos`
+--
 ALTER TABLE `kb_photos`
  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `kb_rate`
+--
+-- Indexen voor tabel `kb_ratings`
+--
+ALTER TABLE `kb_ratings`
  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexen voor tabel `kb_users`
+--
 ALTER TABLE `kb_users`
  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
 
+--
+-- AUTO_INCREMENT voor een tabel `kb_groups`
+--
 ALTER TABLE `kb_groups`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `kb_photos`
+--
 ALTER TABLE `kb_photos`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `kb_rate`
+--
+-- AUTO_INCREMENT voor een tabel `kb_ratings`
+--
+ALTER TABLE `kb_ratings`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `kb_users`
+--
 ALTER TABLE `kb_users`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
