ffcb328e1f8cb25b7a60ed668147d6f30eb4a7e4
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-03-29 03:01:42 +0200

    added eventhandler

--- _js/view/PregnancyView.js
+++ _js/view/PregnancyView.js
@@ -1,16 +1,27 @@
 var template = require('../../_hbs/pregnancy.hbs');
 
 var PregnancyView = Backbone.View.extend({
-	template: template,
+    template: template,
 
-	initialize: function() {
-		_.bindAll.apply(_, [this].concat(_.functions(this)));
-	},
+    initialize: function(options) {
+        this.options = options;
+        _.bindAll.apply(_, [this].concat(_.functions(this)));
 
-	render: function() {
-		this.$el.html(this.template());
-		return this;
-	},
+        this.user = this.options.user;
+    },
+
+    events: {
+        'submit': 'save'
+    },
+
+    save: function(e) {
+        e.preventDefault();
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.user.toJSON()));
+        return this;
+    },
 });
 
 module.exports = PregnancyView;
